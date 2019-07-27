18fcf9c8ee476c9883418344ea3527b76bfb363f
Deepika Thakkar <deepikat@synerzipune.local>
2019-07-27 18:13:58 +0530

    updated msg for new

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -5,7 +5,7 @@ public class App
     {  
     for (int i=1 ;i<=10 ;i++)
     {
-        System.out.println( "This is my Attempt =" + i );
+        System.out.println( "This is my new Attempt =" + i );
     }
     
     }
